868272489a0205b10e350200184c8e28fb0708f4
Rory Glynn <ankhwatcher@gmail.com>
2013-01-17 11:36:22 +0000

    Added comment explaining the purpose of FireableLocation.

--- src/ie/appz/popupplaces/FireableLocation.java
+++ src/ie/appz/popupplaces/FireableLocation.java
@@ -2,6 +2,10 @@ package ie.appz.popupplaces;
 
 import android.location.Location;
 
+/* FireableLocation extends the Location class to have a boolean value mHasFired 
+ * which is accessed with the functions isFired() and setFired().
+ * */
+
 public class FireableLocation extends Location {
 	private boolean mHasFired = false;
 
